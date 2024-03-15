fc07f1506f55da1099686b2d66fa79b22be8b04a
Ahmed Elsayed <ahmed33elsayed22@gmail.com>
2024-03-15 04:28:34 +0200

    Updates to Navbar Component
    Updates to the UI of the navbar component as a initial template for the react component

--- frontend_react/src/Components/Navbar/Navbar.jsx
+++ frontend_react/src/Components/Navbar/Navbar.jsx
@@ -1,5 +1,14 @@
 import React, { useState } from 'react';
-import { AppBar, Toolbar, Typography, Button, Menu, MenuItem } from '@mui/material';
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  Menu,
+  MenuItem,
+  IconButton,
+} from '@mui/material';
+import { AccountCircle } from '@mui/icons-material';
 
 const Navbar = () => {
   const [anchorEl, setAnchorEl] = useState(null);
@@ -13,28 +22,68 @@ const Navbar = () => {
   };
 
   return (
-    <AppBar position="static">
+    <AppBar position="static" sx={{ backgroundColor: '#fff' }}>
       <Toolbar>
-        <Typography variant="h6" style={{ flexGrow: 1 }}>
-          My App
+        <Typography variant="h4" sx={{ flexGrow: 1, fontWeight: 'bold', color: '#0077C2', fontFamily: 'Trebuchet MS' }}>
+          TRVLO
         </Typography>
-        <Button color="inherit" onClick={handleMenuOpen}>
+        <Button
+          color="inherit"
+          onClick={handleMenuOpen}
+          sx={{  fontWeight: 'bold', fontFamily: 'Trebuchet MS', marginRight: 2, borderRadius: 2, color: '#000', '&:hover': { color: '#0077C2' }, textTransform: 'none' }}
+        >
           Articles
         </Button>
         <Menu
           anchorEl={anchorEl}
-          open={Boolean(anchorEl)}
+          open={!!anchorEl}
           onClose={handleMenuClose}
+          elevation={0}
+          getContentAnchorEl={null}
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'left',
+          }}
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'left',
+          }}
+          PaperProps={{
+            sx: {
+              borderRadius: 2,
+              marginTop: '8px',
+              backgroundColor: '#fff',
+              color: '#000',
+              '& .MuiMenuItem-root': {
+                fontWeight: 'bold',
+                '&:hover': {
+                  backgroundColor: '#0077C2',
+                  color: '#fff',
+                },
+             },
+            },
+          }}
         >
           <MenuItem onClick={handleMenuClose}>Article 1</MenuItem>
           <MenuItem onClick={handleMenuClose}>Article 2</MenuItem>
           <MenuItem onClick={handleMenuClose}>Article 3</MenuItem>
           <MenuItem onClick={handleMenuClose}>More Articles</MenuItem>
         </Menu>
-        <Button color="inherit">Destinations</Button>
-        <Button color="inherit">Planning</Button>
-        <Button color="inherit">Challenges</Button>
-        <Button color="inherit">Social</Button>
+        <Button color="inherit" sx={{ fontWeight: 'bold', fontFamily: 'Trebuchet MS', marginRight: 2, color: '#000', '&:hover': { color: '#0077C2' }, textTransform: 'none' }}>
+          Destinations
+        </Button>
+        <Button color="inherit" sx={{ fontWeight: 'bold', fontFamily: 'Trebuchet MS', marginRight: 2, color: '#000', '&:hover': { color: '#0077C2' }, textTransform: 'none' }}>
+          Planning
+        </Button>
+        <Button color="inherit" sx={{ fontWeight: 'bold', fontFamily: 'Trebuchet MS', marginRight: 2, color: '#000', '&:hover': { color: '#0077C2' }, textTransform: 'none' }}>
+          Challenges
+        </Button>
+        <Button color="inherit" sx={{ fontWeight: 'bold', fontFamily: 'Trebuchet MS', marginRight: 2, color: '#000', '&:hover': { color: '#0077C2' }, textTransform: 'none' }}>
+          Social
+        </Button>
+        <IconButton color="inherit" sx={{ marginLeft: 'auto' }} aria-label="account of current user">
+          <AccountCircle sx={{ color: '#000' }} />
+        </IconButton>
       </Toolbar>
     </AppBar>
   );
