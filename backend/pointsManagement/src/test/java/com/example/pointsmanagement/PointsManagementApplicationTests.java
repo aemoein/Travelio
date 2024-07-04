3ddf3489a223445e20fce418426fc34cab419a7a
youstinaSherif <tina.sherif2100@gmail.com>
2024-07-04 22:43:58 +0300

    Points Management

---
+++ backend/pointsManagement/src/test/java/com/example/pointsmanagement/PointsManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pointsmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointsManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
