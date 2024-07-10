94bad1fced14f4cb8adef5a5f0753e919501a159
Ahmed Elsayed <ahmed33elsayed22@gmail.com>
2024-07-10 22:40:01 +0300

    Delete Service

--- backend/Itinerary/Itinerary/src/main/java/com/example/travelio/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.travelio.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
--- backend/Itinerary/Itinerary/src/main/java/com/example/travelio/repository/ItineraryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.travelio.repository;
-
-import com.example.travelio.model.Itinerary;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ItineraryRepository extends MongoRepository<Itinerary, String> {
-}
