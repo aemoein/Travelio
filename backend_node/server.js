0cc5a00fcd8e0b9467fff760b703fd37fd26d06b
Ahmed Elsayed <ahmed33elsayed22@gmail.com>
2024-09-01 03:41:50 +0300

    Session Debug 4

--- backend_node/server.js
+++ backend_node/server.js
@@ -89,9 +89,10 @@ app.use(morgan('dev'));
       resave: false,
       saveUninitialized: false,
       cookie: {
-        secure: process.env.NODE_ENV === 'production',
-        httpOnly: true,
-        maxAge: 30 * 60 * 1000
+        //secure: process.env.NODE_ENV === 'production',
+        //httpOnly: true,
+        maxAge: 30 * 60 * 1000,
+        sameSite: 'None',
       }
     }));
 
